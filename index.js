981123a0b39ded9b486248dc4ea8d4f2b03b6470
Henri Beck <henribeck.dev@gmail.com>
2018-10-21 21:42:41 +0200

    Fix config and add git pre-commit hook

--- index.js
+++ index.js
@@ -20,18 +20,18 @@ module.exports = {
     'no-continue': 'off',
     'flowtype/define-flow-type': 1,
     'flowtype/use-flow-type': 1,
-    'no-console': [1, {allow: ['warn', 'error', 'info']}],
+    'no-console': [1, { allow: ['warn', 'error', 'info'] }],
     'import/no-extraneous-dependencies': 'off',
-    'no-param-reassign': ['error', {props: false}],
+    'no-param-reassign': ['error', { props: false }],
     'react/destructuring-assignment': 'off',
-    'prefer-destructuring': 'off'
+    'prefer-destructuring': 'off',
   },
   parserOptions: {
     ecmaVersion: 8,
     sourceType: 'module',
     ecmaFeatures: { experimentalObjectRestSpread: true },
   },
-  files: [{
+  overrides: [{
     files: ['*.md'],
     rules: {
       'no-unused-vars': 'off',
@@ -39,7 +39,7 @@ module.exports = {
       'react/button-has-type': 'off',
       'react/prop-types': 'off',
       'jsx-a11y/label-has-for': 'off',
-      'jsx-a11y/label-has-associated-control': 'off'
-    }
+      'jsx-a11y/label-has-associated-control': 'off',
+    },
   }],
 };
